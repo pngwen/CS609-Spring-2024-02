69d6e99495b9c34ef69cb44048824cbbb6512756
venkataNareshkollu <45447262+venkataNareshkollu@users.noreply.github.com>
2024-03-20 05:20:44 +0000

    one node updated

--- Regex/one_node.cpp
+++ Regex/one_node.cpp
@@ -1,6 +1,7 @@
 // File: one_node.cpp
 // Purpose: The one node matches the one or more quantifier.
 // Author: Robert Lowe
+//Edula Ranga Venkata Sai Kumar Reddy, Naresh Kollu, Sai Manideep Pellimari
 #include "one_node.h"
 #include <string>
 
@@ -12,17 +13,16 @@ OneNode::~OneNode() { delete _node; }
 
 // Attempt to match the string beginning at the given position.
 bool OneNode::match(const std::string &str, size_t &pos) {
-  size_t start_pos = pos;
-  bool matched_once = false;
+  size_t initialPos = pos;
 
-  while (pos < str.length() && _node->match(str, pos)) {
-    matched_once = true;
-  }
-
-  if (matched_once) {
-    return true;
-  } else {
-    pos = start_pos;
+  // Attempt to match the node at least once
+  if (!_node->match(str, pos))
     return false;
+
+  // Continue attempting to match the node zero or more times
+  while (_node->match(str, pos) && pos < str.length()) {
+    // Increment position if successful match
+    pos++;
   }
+  return (pos > initialPos);
 }
